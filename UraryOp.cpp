16ac427c66da3428b2498a707e3bd819f339a4ce
Saurabh Shakya <shakyasaurabh951@gmail.com>
2025-09-22 10:53:33 +0530

    Add file

---
+++ UraryOp.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+  public :
+    int feet,inch;
+
+    A(int f,int i)
+    {
+        this->feet =f;
+        this->inch =i;
+    }
+    void operator-()
+    {
+        feet++;
+        inch++;
+        cout<<"\nfeet & inches(Decrement):"<<feet<<""<<inch;
+    }
+};
+
+int main()
+{
+    A d1(8,9);
+
+    -d1;
+    return 0;
+}
