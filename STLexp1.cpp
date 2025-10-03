2eed8389f3e16fba39e480c273f76cbd2ee71db7
Saurabh Shakya <shakyasaurabh951@gmail.com>
2025-10-03 10:47:53 +0530

    add commit

--- STLexp1.cpp
+++ STLexp1.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main()
 {
-    array<int,5> a = {23,56,87,96,34};
+    array<int,10> a = {23,56,87,96,34,89,54,12,10,98};
+
+    /*int c =count(a.begin(), a.end());
+    cout<<"Count value is: "<<endl;*/
+    //auto mvalue = max_element(a.begin(), a.end());
+    //auto mvalue = min_element(a.begin(), a.end());
+   /* auto newData = remove(a.begin(), a.end(), 89);
+    int d = distance(a.begin(), newData);
+
+    for (int i=0; i<d; i++)
+    {
+        cout<<a[i]<<"";
+    }*/
 
 
     for(int i=0; i<5; i++)
