82dfb32c2ec44a6e3ba81542c57c59bf0c03752d
Saurabh Shakya <shakyasaurabh951@gmail.com>
2025-10-30 09:54:37 +0530

    Add commit

--- Linklist.cpp
+++ Linklist.cpp
@@ -64,6 +64,112 @@ void insertAtPosition(int value, int position) {
     cout << "Inserted " << value << " at position " << position << ".\n";
 }
 
+//delete first
+void deletefirst()
+{
+    if(head == NULL)
+    {
+        cout<<"List is Empty"<<endl;
+    }
+    else
+    {
+        Node*temp = head;
+        head = head->next;
+        delete temp;
+        cout<<"First node is deleted"<<endl;
+    }
+}
+
+//delete last
+void deletelast()
+{
+    if(head==NULL)
+    {
+        cout<<"List is empty"<<endl;
+    }
+    else if(head->next == NULL)
+    {
+       delete head;
+       head = NULL;
+       cout<<"Only one node in list"<<endl; 
+    }
+    else
+    {
+        Node*temp = head;
+        Node*prev = NULL;
+        while(temp->next!= NULL)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = NULL;
+        delete temp;
+        cout<<"Last node is deleted"<<endl;
+    }
+}
+
+//delete at position
+
+void deleteAtposition(int position)
+{
+    if (head == NULL)
+    {
+        cout<<"List is empty"<<endl;
+    }
+    else if(head->next == NULL)
+    {
+        delete head;
+        head = NULL;
+        cout<<"Only one Node"<<endl;
+    }
+    else
+    {
+      Node*temp = head;
+      for(int i = 0; i< position;i++)
+      {
+        if (temp == NULL)
+        {
+            cout<<"Out of Range"<<endl;
+            return;
+        }
+        temp = temp->next;
+      }
+      Node*delNode = temp->next;
+      if(delNode == NULL)
+      {
+        cout<<"No Node is Exist"<<endl;
+      }
+      temp->next=delNode->next;
+      delete delNode;
+      cout<<"Node delete at position"<<endl;
+    }
+}
+
+//Search
+
+void search(int value)
+{
+    Node*temp = head;
+    bool f = false;
+    int i = 0;
+
+    while(temp!=NULL)
+    {
+        if(temp->data==value)
+        {
+            cout<<"Record found at Position : "<<(i+1)<<endl;
+            f = true;
+            break;
+        }
+        temp=temp->next;
+        i++;
+    }
+    if(!f)
+    {
+        cout<<"Record not found"<<endl;
+    }
+}
+
 // Function to display the linked list
 void display() {
     Node* temp = head;
@@ -89,8 +195,12 @@ int main() {
         cout << "1. Insert at Beginning\n";
         cout << "2. Insert at End\n";
         cout << "3. Insert at Any Position\n";
-        cout << "4. Display\n";
-        cout << "5. Exit\n";
+        cout << "4. Delete at first\n";
+        cout << "5. Delete at Last\n";
+        cout << "6. Delete at position\n";
+        cout << "7. Search\n";
+        cout << "8. Display\n";
+        cout << "9. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -112,14 +222,36 @@ int main() {
                 cin >> value >> position;
                 insertAtPosition(value, position);
                 break;
-
+            
             case 4:
+                 deletefirst();
+                 break;
+                 
+            case 5:
+                 deletelast();
+                 break;
+                 
+            case 6:
+                cout<<"Enter Position:";
+                cin>> position;
+                deleteAtposition(position);
+                break;  
+                
+            case 7:
+               cout<<"Enter Value : ";
+               cin >> value;
+               search(value);
+               break;    
+
+
+            case 8:
                 display();
                 break;
 
-            case 5:
+            case 9:
                 cout << "Exiting...\n";
                 return 0;
+               
 
             default:
                 cout << "Invalid choice! Try again.\n";
